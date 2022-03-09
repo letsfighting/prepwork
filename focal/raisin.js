df4ef60e93897fff26a34f384f6872f61ba056d7
Elroy Hui <huielroy@hotmail.com>
2022-03-09 18:29:39 -0500

    implemented raisinArray stretch

--- focal/raisin.js
+++ focal/raisin.js
@@ -7,8 +7,35 @@ const raisinAlarm = function (cookie) {
   return "All good!";
 };
 
+const raisinAlarmArray = function (cookie) {
+  let results = [];
+
+  //nested for to iterate through each value of the nested array
+
+  for (let i = 0; i < cookie.length; i++) {
+    for (let j = 0; j < cookie[i].length; j++) {
+      //determing if the element contained in the nested array contains a match to raisins
+      if (cookie[i][j] === "🍇") {
+        results.push("Raisin Alert!");
+        break;
+        //determine if this iteration is the last item in the array and push all good if no raisins have been detected
+      } else if (j + 1 === cookie[i].length) {
+        results.push("All good!");
+        break;
+      }
+    }
+  }
+  return results;
+};
+
 console.log(raisinAlarm(["🍫", "🍫", "🍇", "🍫"]));
 console.log(raisinAlarm(["🍫", "🍇", "🍫", "🍫", "🍇"]));
 console.log(raisinAlarm(["🍫", "🍫", "🍫"]));
 
-let r = ["🍫", "🍫", "🍇", "🍫"];
+console.log(
+  raisinAlarmArray([
+    ["🍫", "🍫", "🍇", "🍫"],
+    ["🍫", "🍇", "🍫", "🍫", "🍇"],
+    ["🍫", "🍫", "🍫"],
+  ])
+);
